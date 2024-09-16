146ccbacd38befdd02f61520aef6272a62040803
Lefie <qiwenruan11@gmail.com>
2024-09-16 17:34:29 -0400

    forms done

--- backend/index.js
+++ backend/index.js
@@ -6,33 +6,107 @@ const app = express()
 const port = 8080
 
 
-// other functions 
-const conversion_length = (value, from, to) => {
-    
-   const conversionFactors = {
-    millimeter: 0.001,
-    centimeter: 0.01,
-    meter: 1,
-    kilometer: 1000,
-    inch: 0.0254,
-    foot: 0.3048,
-    yard: 0.9144,
-    mile: 1609.344
-   }
 
-   if ((!from in conversionFactors) || !( to in conversionFactors)){
-        throw new Error("invalid unit");
-   }
+// helper functions 
+/*
 
-   // convert the value to meters
-   let valueInMeters = value * conversionFactors[from]
+value : the value which user inputs to be converted 
+from : unit to convert from
+to : unit to convert to
+type :  the type of conversion, length, weight or temp
+*/
+const conversion = (value, from, to, type) => {
+    const conversionFactors_length = {
+        millimeter: 0.001,
+        centimeter: 0.01,
+        meter: 1,
+        kilometer: 1000,
+        inch: 0.0254,
+        foot: 0.3048,
+        yard: 0.9144,
+        mile: 1609.344
+    }
 
-   //convertedValue
-   let convertedValue = valueInMeters / conversionFactors[to]
+    const convertsionFactors_weight = {
+        milligram: 0.001,
+        gram: 1,
+        kilogram: 1000,
+        ounce: 28.3495,
+        pound: 453.592
+    }
 
-   return Math.round(convertedValue * 10000) / 10000
 
+    if (type === "length"){
+        
+        if ((!from in conversionFactors_length) || !( to in conversionFactors_length)){
+            throw new Error("invalid unit");
+        }
+        console.log((value), conversionFactors_length[from] )
+
+         // convert the value to meters
+        let valueInMeters = value * conversionFactors_length[from]
+        console.log("I am in length", valueInMeters)
+
+        //convertedValue
+        let convertedValue = valueInMeters / conversionFactors_length[to]
+
+        return Math.round(convertedValue * 10000) / 10000
+    }
+
+    if ( type === "weight"){
+        if ((!from in convertsionFactors_weight) || !( to in convertsionFactors_weight)){
+            throw new Error("invalid unit");
+        }
+
+        // Convert value from the initial unit to grams (base unit)
+        let valueInGrams = value * convertsionFactors_weight[from];
+
+        let convertedValue = valueInGrams / convertsionFactors_weight[to];
+
+        return Math.round(convertedValue * 10000) / 10000;
+
+    }
+
+    if ( type === "temp") {
+        console.log("from temp, from and to", value, from, to)
+        // convert the 'from' value to celcius 
+        let celciusVal;
+        switch (from){
+            case 'celsius':
+                celciusVal = value
+                break
+            case 'fahrenheit':
+                celciusVal = (value - 32) * 5 / 9;
+                break
+            case 'kelvin':
+                celciusVal = value - 273.15;
+                break
+            default:
+                throw new Error('Invalid from unit');
+
+        }
+        console.log("celciusVAl", celciusVal)
+
+        let convertedVal;
+        switch(to) {
+            case 'celsius':
+                convertedVal = celciusVal;
+                break;
+            case 'fahrenheit':
+                convertedVal = (celciusVal * 9 / 5) + 32;
+                break;
+            case 'kelvin':
+                convertedVal = celciusVal + 273.15;
+                break;
+            default:
+                throw new Error('Invalid to unit')
+        }
+        
+        return Math.round(convertedVal * 10000)  / 10000
+        }
 }
+
+
 
 app.use(bodyParser.urlencoded({extended:true}))
 // parse application/json
@@ -56,10 +130,43 @@ app.post("/length", function(req,res) {
     if( from_value === to_value){
         res.json({"result":length})
     }else{
-        converted_value = conversion_length(length,from_value,to_value)
+        let converted_value = conversion(length,from_value,to_value, "length")
         console.log("converted value is",converted_value)
         res.json({"result":converted_value})
     }
+})
+
+app.post("/weight", function(req, res) {
+    fp = path.join(__dirname,'../frontend/index.html')
+    
+    let weight = parseFloat(req.body.weight)
+    let from_value = req.body.from
+    let to_value = req.body.to
+
+    if( from_value === to_value){
+        res.json({"result":weight})
+    }else {
+        let converted_value = conversion(weight,from_value, to_value, "weight")
+        console.log(converted_value)
+        res.json({"result":converted_value})
+    }
+})
+
+app.post("/temperature", function(req, res){
+    fp = path.join(__dirname,'../frontend/index.html')
+
+    let temp = parseFloat(req.body.temp)
+    let from = req.body.from
+    let to = req.body.to
+
+    if (from === to) {
+        res.json({"result":temp})
+    }else {
+        let converted_value = conversion(temp, from, to, "temp")
+        console.log(converted_value)
+        res.json({"result": converted_value})
+    }
+
 })
 
 app.listen(port)
--- frontend/app.js
+++ frontend/app.js
@@ -11,6 +11,8 @@ console.log(length_label, weight_label, temp_label )
 
 weight_label.addEventListener("click", function (event) {
     event.preventDefault()
+
+    // switch 'selected' to weight form
     if (length_label.classList.contains("selected")){
         length_label.classList.remove("selected")
     }
@@ -21,18 +23,18 @@ weight_label.addEventListener("click", function (event) {
 
     weight_label.classList.add("selected")
 
-    const ele_to_remove = forms.children[0]
-    if (ele_to_remove){
-        forms.removeChild(ele_to_remove)
+    // console.log(forms.children)
+
+    // remove the current elements 
+    while (forms.children.length != 0){
+        forms.children[0].remove()
     }
 
-    const node = document.createElement("p")
-    node.innerHTML = "Weight"
-    forms.appendChild(node)
+    // form
+    const form = generate_form("weight")
+    
+    forms.appendChild(form)
 
-
-    weight_label.classList.add("selected")
-    console.log("weight")
 
 })
 
@@ -48,14 +50,12 @@ length_label.addEventListener("click", function (event) {
     length_label.classList.add("selected")
     console.log("length")
 
-    const ele_to_remove = forms.children[0]
-    if (ele_to_remove){
-        forms.removeChild(ele_to_remove)
+    while (forms.children.length != 0){
+        forms.children[0].remove()
     }
 
-    const node = document.createElement("p")
-    node.innerHTML = "Length"
-    forms.appendChild(node)
+    const form = generate_form("length")
+    forms.appendChild(form)
 })
 
 
@@ -69,17 +69,17 @@ temp_label.addEventListener("click", function (event) {
     if (weight_label.classList.contains("selected")){
         weight_label.classList.remove("selected")
     }
+
     temp_label.classList.add("selected")
     console.log("temp")
 
-    const ele_to_remove = forms.children[0]
-    if (ele_to_remove){
-        forms.removeChild(ele_to_remove)
+    while (forms.children.length != 0){
+        forms.children[0].remove()
     }
 
-    const node = document.createElement("p")
-    node.innerHTML = "Temp"
-    forms.appendChild(node)
+    const form = generate_form("temp")
+    forms.appendChild(form)
+
 })
 
 /*
@@ -214,17 +214,232 @@ const generate_form = (type) => {
 
     }
 
-    return form
+    if ( type === "weight") {
+        
+        // label 
+        let label_node = generate_node("label",[{"for":"weight"}],"Enter the weight to convert")
+        form.appendChild(label_node)
 
+        // input bar
+        let input_node = generate_node("input",[{"type":"number"},{"id":"weight"},{"name":"weight"}])
+        form.appendChild(input_node)
+
+         // line break
+         const line_break = document.createElement("br")
+         form.appendChild(line_break)
+
+        // label 2
+        let label_node1 = generate_node("label",[{"for":"from"}],"<br><br>Unit to Convert <strong>from</strong>")
+        form.appendChild(label_node1)
+
+        // select node 
+        let select_node = generate_node("select",[{"name":"from"},{"id":"from"}])
+        // option node 
+        let option1 = generate_node("option",[{"value":"milligram"}],"milligram")
+        let option2 = generate_node("option",[{"value":"gram"}],"gram")
+        let option3 = generate_node("option",[{"value":"kilogram"}],"kilogram")
+        let option4 = generate_node("option",[{"value":"ounce"}],"ounce")
+        let option5 = generate_node("option",[{"value":"pound"}],"pound")
+
+        select_node.appendChild(option1)
+        select_node.appendChild(option2)
+        select_node.appendChild(option3)
+        select_node.appendChild(option4)
+        select_node.appendChild(option5)
+    
+        form.appendChild(select_node)
+
+        //label 3
+        let label_node2 = generate_node("label",[{"for":"from"}],"<br><br>Unit to Convert <strong>to</strong>")
+        form.appendChild(label_node2)
+
+        // select node 
+        let select_node2 = generate_node("select",[{"name":"to"},{"id":"to"}])
+        
+        // options
+        let option9 = generate_node("option",[{"value":"milligram"}],"milligram")
+        let option10 = generate_node("option",[{"value":"gram"}],"gram")
+        let option11 = generate_node("option",[{"value":"kilogram"}],"kilogram")
+        let option12 = generate_node("option",[{"value":"ounce"}],"ounce")
+        let option13 = generate_node("option",[{"value":"pound"}],"pound")
+
+
+        select_node2.appendChild(option9)
+        select_node2.appendChild(option10)
+        select_node2.appendChild(option11)
+        select_node2.appendChild(option12)
+        select_node2.appendChild(option13)
+  
+        form.appendChild(select_node2)
+
+        // line break
+        form.appendChild(line_break)
+
+
+        // convert button 
+        let convert_btn = generate_node("input",[{"type":"submit"},{"value":"Convert"}])
+
+        convert_btn.addEventListener("click", function(event){
+            event.preventDefault()
+            const weight = document.getElementById("weight").value
+            const from = document.getElementById("from").value
+            const to = document.getElementById("to").value
+            
+            fetch("/weight", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    weight:weight,
+                    from : from,
+                    to: to
+                })
+            })
+            .then(response => response.json())
+            .then(data => {
+                const weight_result = generate_node("p",[{"id":"result"}],`the result of your calculation <br> ${weight} ${from} = ${data.result} ${to}`)
+                forms.replaceChild(weight_result, form)
+
+                //create a reset button
+                const btn = generate_node("button",[{"id":"reset"}],"Reset")
+
+                btn.addEventListener("click", function(event){
+                    event.preventDefault()
+                    forms.removeChild(btn)
+                    forms.replaceChild(form, weight_result)
+                })
+
+                forms.appendChild(btn)
+            })
+            .catch(error => {
+                const error_obj = document.createElement("p")
+                error_obj.innerHTML = error
+                forms.appendChild(error_obj)
+            })
+
+        })
+
+            form.appendChild(convert_btn)
+    }
+
+    if (type === "temp") {
+        // label 
+        let label_node = generate_node("label",[{"for":"temp"}],"Enter the temprature to convert")
+        form.appendChild(label_node)
+
+        // input bar
+        let input_node = generate_node("input",[{"type":"number"},{"id":"temp"},{"name":"temp"}])
+        form.appendChild(input_node)
+
+         // line break
+         const line_break = document.createElement("br")
+         form.appendChild(line_break)
+
+        // label 2
+        let label_node1 = generate_node("label",[{"for":"from"}],"<br><br>Unit to Convert <strong>from</strong>")
+        form.appendChild(label_node1)
+
+        // select node 
+        let select_node = generate_node("select",[{"name":"from"},{"id":"from"}])
+
+        // option node 
+        let option1 = generate_node("option",[{"value":"celsius"}],"celsius")
+        let option2 = generate_node("option",[{"value":"fahrenheit"}],"fahrenheit")
+        let option3 = generate_node("option",[{"value":"kelvin"}],"kelvin")
+ 
+        select_node.appendChild(option1)
+        select_node.appendChild(option2)
+        select_node.appendChild(option3)
+
+        form.appendChild(select_node)
+
+        //label 3
+        let label_node2 = generate_node("label",[{"for":"to"}],"<br><br>Unit to Convert <strong>to</strong>")
+        form.appendChild(label_node2)
+
+        // select node 
+        let select_node2 = generate_node("select",[{"name":"to"},{"id":"to"}])
+
+        // option node 
+        let option4 = generate_node("option",[{"value":"celsius"}],"celsius")
+        let option5 = generate_node("option",[{"value":"fahrenheit"}],"fahrenheit")
+        let option6 = generate_node("option",[{"value":"kelvin"}],"kelvin")
+ 
+        select_node2.appendChild(option4)
+        select_node2.appendChild(option5)
+        select_node2.appendChild(option6)
+
+        form.appendChild(select_node2)
+
+        // line break
+        form.appendChild(line_break)
+
+        // convert button 
+        let convert_btn = generate_node("input",[{"type":"submit"},{"value":"Convert"}])
+
+        convert_btn.addEventListener("click", function(event) {
+            event.preventDefault()
+            const temp = document.getElementById("temp").value
+            const from = document.getElementById("from").value
+            const to = document.getElementById("to").value
+
+            console.log("clieant", temp, from, to)
+
+            fetch("/temperature",{
+               method: "POST",
+               headers: {
+                'Content-Type': 'application/json'
+               },
+               body: JSON.stringify({
+                temp:temp,
+                from:from,
+                to:to
+               })
+                
+            })
+            .then(response => response.json())
+            .then(data => {
+                const temp_result = generate_node("p", [{"id":"result"}], `the result of your calculation <br> ${temp} ${from} : ${data.result}${to}`)
+                forms.replaceChild(temp_result, form)
+
+                // create a reset button 
+                const btn = generate_node("button",[{"id":"reset"}], "Reset")
+
+                btn.addEventListener("click", function(event){
+                    event.preventDefault()
+                    forms.removeChild(btn)
+                    forms.replaceChild(form, temp_result)
+                })
+
+                forms.appendChild(btn)
+                
+            })
+            .catch(error => {
+                const error_obj = document.createElement("p")
+                error_obj.innerHTML = error
+                forms.appendChild(error_obj)
+            })
+
+        })
+            form.appendChild(convert_btn)
+
+
+    }
+
+    return form;
 }
+
 
 /* 
 type: the name of the node , str
 attributes: [{key, value}] -> a list of dictionaries 
+content : string
 */
 
 const generate_node = (type, attributes, content) => {
     let node = document.createElement(type)
+    console.log("node")
     for (let i = 0; i < attributes.length; i++){
         const key = Object.keys(attributes[i])[0]
         const value = attributes[i][key]
@@ -235,18 +450,10 @@ const generate_node = (type, attributes, content) => {
 }
 
 
-
-
 if(length_label.classList.contains("selected")){
     //generate_form("length")
     console.log("length is selected")
     const length_form = generate_form("length")
     forms.appendChild(length_form)
-    
 } 
-
-
-
-
-  
 
